db5d99830c92b76b44199c81569a334bfca7037a
zane-x <zane.x@binance.com>
2023-10-26 22:59:50 +0900

    feat: update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 // require("dotenv").config()
 module.exports = {
   siteMetadata: {
-    titleAlt: 'My super blog',
-    headline: 'My super blog',
-    url: 'My super blog',
-    description: 'My super blog',
-    author: 'Strapi team',
-    title: 'Mimigon'
+    titleAlt: "My super blog",
+    headline: "My super blog",
+    url: "My super blog",
+    description: "My super blog",
+    author: "Strapi team",
+    title: "Mimigon",
   },
   plugins: [
-    'gatsby-plugin-theme-ui',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-postcss",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -21,16 +21,17 @@ module.exports = {
       },
     },
     {
-      resolve: '@xuzhanhh/gatsby-source-strapi',
+      resolve: "@xuzhanhh/gatsby-source-strapi",
       options: {
         apiURL: process.env.DEPLOY_URL
           ? "http://admin.mimigon.com"
           : "http://localhost:1337",
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
-          'article',
-          'user',
-          'section',
-          'about'
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "article",
+          "user",
+          "section",
+          "about",
         ],
         queryLimit: 1000,
       },
@@ -38,8 +39,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ['G-MH03DSZDN3']
-      }
+        trackingIds: ["G-MH03DSZDN3"],
+      },
     },
     // {
     //   resolve: "gatsby-plugin-firebase",
@@ -55,28 +56,20 @@ module.exports = {
     //     }
     //   }
     // },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'mimigon',
-        short_name: 'mimigon',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/mimigonlogo_raw.png', // This path is relative to the root of the site.
+        name: "mimigon",
+        short_name: "mimigon",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/assets/mimigonlogo_raw.png", // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-remove-serviceworker`,
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: 'QEOOUNYNM3',
-        apiKey: '6cd9e2ec519298e746e0d3bbd7f90205',
-        queries: require("./src/utils/algolia-queries")
-      },
-    }
   ],
 }
